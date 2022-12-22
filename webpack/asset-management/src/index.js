7b18accd8ae15c872e6abeabb76af11ed9d44ece
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-22 08:09:46 +0200

    Added toml yaml json5 data loading examples

--- webpack/asset-management/src/index.js
+++ webpack/asset-management/src/index.js
@@ -1,16 +1,35 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+import { name } from './data.json';
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
 
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+  // Adding styles
   element.classList.add('hello');
 
+  // Adding images
   const image = new Image();
   image.src = Icon;
   element.appendChild(image);
+
+  // Adding data
+  console.log(Data);
+  console.log(Notes);
+  console.log(name);
+
+  // Adding other data formats
+  console.log(toml);
+  console.log(yaml);
+  console.log(json);
 
   return element;
 }
