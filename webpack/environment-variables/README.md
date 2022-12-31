399a9ce042b1b6f08d5a089474837fd92aafbe66
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-31 07:21:09 +0200

    Added environment variables example

--- webpack/environment-variables/README.md
+++ webpack/environment-variables/README.md
@@ -1,1 +1,6 @@
 More info: https://webpack.js.org/guides/environment-variables/
+
+
+## Notes 
+There is one change that you will have to make to your webpack config. Typically, `module.exports` points to the configuration object.
+To use the `env` variable, you must convert `module.exports` to a function.
--- webpack/environment-variables/webpack.config.js
+++ webpack/environment-variables/webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+
+/*
+  There is one change that you will have to make to your webpack config. Typically, `module.exports` points to the configuration object.
+  To use the `env` variable, you must convert `module.exports` to a function.
+*/
+
+module.exports = (env) => {
+  console.log('Goal:', env.goal); // local
+  console.log('Production:', env.production); // true
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  }
 };
